11ac9a49c985f48ae34131e6f4b72a6cf972e5cc
Daphne Watson <daphne_watson@Daphnes-MacBook-Air.local>
2015-05-13 18:54:10 -0700

    Added red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS goes here
+// newDiv.innerHTML = "";
+
+for (var i = 0; i < 81; i++) {
+var newDiv = document.createElement("div");
+
+document.body.appendChild(newDiv);
+// newDiv.style.backgroundColor = "red";
+newDiv.style.width = "11.1%";
+newDiv.style.float = "left";
+newDiv.style.padding = "0 0 11.1% 0";
+
+	if (i % 2 === 0) {
+		newDiv.style.backgroundColor = "red";
+	}
+	else {
+		newDiv.style.backgroundColor = "black";
+	}
+}
+
+
+// newDiv.style.float = "left";
+
+// var blackDiv = document.createElement("div");
+// blackDiv.innerHTML = "square";
+
+// document.body.appendChild(newDiv)

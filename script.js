5d6a1890ad35ff8a807cb60ca1ced2c455c9fa18
Daphne Watson <daphne_watson@Daphnes-MacBook-Air.local>
2015-05-13 20:32:05 -0700

    Attempted to write gradient code

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your JS goes here
+
+// for (var i = 0; i < 81; i++) {
+
+var newDiv = document.createElement("div");
+
+document.body.appendChild(newDiv);
+// newDiv.style.backgroundColor = "red";
+newDiv.style.width = "11.1%";
+newDiv.style.float = "left";
+newDiv.style.padding = "0 0 11.1% 0";
+
+for (var i = 0; i < 81; i++) {
+for (var j = 0; j < 81; j++) {
+	if (i % 2 === 0) {
+		newDiv.style.backgroundColor = (0, 244, 0, 1);
+	}
+	else {
+		newDiv.style.backgroundColor = "#66cdaa";
+	}
+	}
+	}
+}

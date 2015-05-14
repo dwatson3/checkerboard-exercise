bb94220962b76abd39421fe094969a6e52f0e514
Daphne Watson <daphne_watson@Daphnes-MacBook-Air.local>
2015-05-13 19:24:02 -0700

    added random colors

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your JS goes here
+for (var i = 0; i < 81; i++) {
+var newDiv = document.createElement("div");
+
+document.body.appendChild(newDiv);
+newDiv.style.width = "11.1%";
+newDiv.style.float = "left";
+newDiv.style.padding = "0 0 11.1% 0";
+newDiv.style.backgroundColor = "#" + Math.floor(Math.random()*0xFFFFFF<<0).toString(16);
+
+// (newDiv)+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+
+// for (var i = 0; i < 81; i++) {
+// var newDiv = document.createElement("div");
+
+// document.body.appendChild(newDiv);
+// // newDiv.style.backgroundColor = "red";
+// newDiv.style.width = "11.1%";
+// newDiv.style.float = "left";
+// newDiv.style.padding = "0 0 11.1% 0";
+
+// 	if (i % 2 === 0) {
+// 		newDiv.style.backgroundColor = "red";
+// 	}
+// 	else {
+// 		newDiv.style.backgroundColor = "black";
+// 	}
+// }
